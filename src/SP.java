2cf5538be13ff08ae69de26cdcc0de811738c3f5
JoelBarros24 <a94692@alunos.uminho.pt>
2022-12-05 18:17:34 +0000

    add new class DataBaseFile

--- src/SP.java
+++ src/SP.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class SP extends Server {
     public ConfigurationFile cf = new ConfigurationFile();
-    public DataBaseFile_new dbf = new DataBaseFile_new(this.cf);
+    public DataBaseFile dbf = new DataBaseFile(this.cf);
 
     public SP(int port, int time_out, String modo_debug, String path) throws IOException {
         super(port, time_out, modo_debug, path);
